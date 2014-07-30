4d9e1ecb75fcb7cb8a2ad77b78b78f85063c4851
Ari Rubinstein <ari.d.rubinstein@gmail.com>
2014-07-30 01:48:00 -0700

    Move to v1.2 api, and also fix srs mapping

--- api.go
+++ api.go
@@ -8,7 +8,7 @@ import "io/ioutil"
 import "container/list"
 
 func GetKanjiForApiKey(key string) *list.List {
-	url := "http://www.wanikani.com/api/user/" + key + "/kanji"
+	url := "https://www.wanikani.com/api/v1.2/user/" + key + "/kanji"
 	fmt.Println("Getting kanji for url " + url)
 
 	response, err := http.Get(url)
@@ -22,28 +22,27 @@ func GetKanjiForApiKey(key string) *list.List {
 	result := list.New()
 
 	var jsonData interface{}
-	json.Unmarshal(content, &jsonData)	
+	json.Unmarshal(content, &jsonData)
 
 	requestedInformation := jsonData.(map[string]interface{})
 
 	for _, v := range requestedInformation {
 		switch v.(type) {
-			case []interface{}:
-				for i := range(v.([]interface{})) {
-					data := v.([]interface{})[i].(map[string]interface{})
-					character := data["character"].(string) 
-					if data["stats"] == nil {
-						continue
-					}
-					stats := data["stats"].(map[string]interface{})
-					srs := stats["srs"].(string)
-					kanjiStats := KanjiStats { srs }
-					kanji := Kanji { character, kanjiStats.Status() }
-
-					result.PushBack(kanji)
+		case []interface{}:
+			for i := range v.([]interface{}) {
+				data := v.([]interface{})[i].(map[string]interface{})
+				character := data["character"].(string)
+				if data["user_specific"] == nil {
+					continue
 				}
-			default:
-				continue
+				srs := data["user_specific"].(map[string]interface{})["srs"].(string)
+				kanjiStats := KanjiStats{srs}
+				kanji := Kanji{character, kanjiStats.Status()}
+
+				result.PushBack(kanji)
+			}
+		default:
+			continue
 		}
 	}
 
